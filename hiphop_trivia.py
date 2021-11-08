79a04f8c6ace28fb3518615dd9b018e5a7198f93
Kris Smith <krisp1424@gmail.com>
2021-11-08 15:42:35 -0800

    imported question dictionary

--- hiphop_trivia.py
+++ hiphop_trivia.py
@@ -4,18 +4,12 @@ music trivia application as python fundamentals project
 utilizing defined functions, loops, data sets, modules, and
 data sets
 """
-
 import random
-
-trivia_questions = {"What is the name of Snoop Dogs first Album?": "Doggy Style", "Where did Lauryn Hill attend college before dropping out to pursue her music career full time?":
-                    "Columbia University", "What was the name of the record label founded by Suge Knight, Dr. Dre, The D.O.C., and Dick Griffey?": "Death Row Records", "Who was the first hip-hop group to get a star on the Hollywood Walk of Fame?": "Cypress Hill"}
-
+from trivia_pkg.questions import trivia_questions
 
 def main():
     user_option = input("""Welcome to Hip Hop Trivia! Would you like to play?
     Type 'yes' to continue, type 'no' to quit: """)
-    num_tries = 0
-    total_score = 0
 
     while True:
         if user_option.lower() == "yes":
@@ -27,6 +21,8 @@ def main():
                 quit()
     input("Get ready for your first question! Press ENTER to continue.")
 
+    total_score = 0
+    num_tries = 0 
     while True:
         while num_tries < 3:
             print(total_score)
